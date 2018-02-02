be05a026558434d9f9cf83d02ff8b674cc5956df
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-02 17:19:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,4 +59,15 @@ class Song
     song
   end
 
+  def self.create_from_filename
+    array = filename.split(" - ")
+    artist_name = array[0]
+    name = array[1].chomp(".mp3")
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+    self.all << song
+  end
+
 end
